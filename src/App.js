610af5f580f57735192ff87d247bc4c6a263e469
Berlie <berl.yiadom@gmail.com>
2022-01-04 23:33:24 +0000

    Created the weather. js component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className='container'>
+      <h1>Weather</h1>
+      <Weather defaultCity=" Accra"/>
+      <footer>
+        This project was coded by Berlie and is <a href='https://github.com/Boakyewaa/react-weather-app' target="_blank" rel='noreferrer' >open-sourced on Github</a>
+      </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import axios from "axios"
+import './Weather.css';
+
+import WeatherInfo from "./WeatherInfo";
+
+export default function Weather(props){
+    const [ready, setReady] = useState(false);
+    const [weatherData, setWeatherData] = useState({ });
+    const [city, setCity] = useState (props.defaultCity);
+    function handleResponse(response){
+        console.log(response.data);
+        setWeatherData({
+            ready: true,
+            temperature: response.data.main.temp,
+            description: response.data.weather[0].description,
+            wind: response.data.wind.speed,
+            city: response.data.name,
+            humidity: response.data.main.humidity,
+            date: new Date(response.data.dt * 1000),
+            icon:response.data.weather[0].icon,
+        });
+        setReady(true);
+
+    }
+    function search(){
+        const apiKey = "1504ebb010471d47f96224deb5dd303e";
+        let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+    }
+    function handleSubmit(event){
+        event.preventDefault();
+        search();
+    }
+
+    if (ready) {
+        function handleCityChange(event){
+            setCity(event.target.value);
+        }
+        return(
+        <div className="Main-weather">
+            <form className="search-button" onSubmit={handleSubmit}>
+                 <input type="text" placeholder="Search for a location" className="search-input" onChange={handleCityChange}/>
+            <input type="button" className="btn btn-primary" value="Search"/>
+            </form>
+            
+               <div className="Weather">
+               <WeatherInfo data={weatherData} /> 
+           </div> 
+        </div>
+    );
+    } else{
+        search();
+        return "Loading..."
+    }
+    
+}
