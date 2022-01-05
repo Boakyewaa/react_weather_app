630f7b353ec59a862edbd931719c878ab4812bc4
Berlie <berl.yiadom@gmail.com>
2022-01-05 00:12:23 +0000

    Edited the api url

--- src/Weather.js
+++ src/Weather.js
@@ -25,7 +25,7 @@ export default function Weather(props){
     }
     function search(){
         const apiKey = "1504ebb010471d47f96224deb5dd303e";
-        let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
         axios.get(apiUrl).then(handleResponse);
     }
     function handleSubmit(event){
